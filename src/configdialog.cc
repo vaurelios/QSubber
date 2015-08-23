125459bcd55c0588c072e03ce28422a771808887
Victor A. Santos <victoraur.santos@gmail.com>
2015-08-23 11:13:54 -0300

    new configdialog class

---
+++ src/configdialog.cc
@@ -0,0 +1,7 @@
+#include "configdialog.h"
+
+configdialog::configdialog()
+{
+
+}
+
---
+++ src/configdialog.h
@@ -0,0 +1,15 @@
+#ifndef CONFIGDIALOG_H
+#define CONFIGDIALOG_H
+
+
+class configdialog : public QDialog
+{
+public:
+    configdialog();
+
+signals:
+
+public slots:
+};
+
+#endif // CONFIGDIALOG_H
